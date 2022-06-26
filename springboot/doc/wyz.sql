65057ad3b20b2fc56510e4b3849afc2453e681eb
ListenerYing <1244746663@qq.com>
2022-06-26 20:18:18 +0800

    数据库更新

--- springboot/doc/wyz.sql
+++ springboot/doc/wyz.sql
@@ -16,6 +16,23 @@ CREATE DATABASE /*!32312 IF NOT EXISTS*/`wyz` /*!40100 DEFAULT CHARACTER SET utf
 
 USE `wyz`;
 
+/*Table structure for table `deadline` */
+
+DROP TABLE IF EXISTS `deadline`;
+
+CREATE TABLE `deadline` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `first` timestamp NULL DEFAULT NULL,
+  `second` timestamp NULL DEFAULT NULL,
+  `third` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Data for the table `deadline` */
+
+insert  into `deadline`(`id`,`first`,`second`,`third`) values 
+(1,'2022-06-28 18:10:19','2022-06-30 18:10:31','2022-07-01 17:10:34');
+
 /*Table structure for table `intention` */
 
 DROP TABLE IF EXISTS `intention`;
@@ -27,22 +44,22 @@ CREATE TABLE `intention` (
   `sequence` int DEFAULT NULL COMMENT '志愿顺序',
   PRIMARY KEY (`id`),
   KEY `sequence` (`sequence`)
-) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 /*Data for the table `intention` */
 
 insert  into `intention`(`id`,`student_id`,`teacher_id`,`sequence`) values 
-(15,2,100,1),
-(16,2,101,2),
-(17,2,102,3),
-(18,2,103,4),
-(19,2,104,5),
+(16,2,101,5),
+(17,2,102,2),
+(18,2,103,1),
+(19,2,104,3),
 (20,5,100,1),
 (21,5,101,3),
 (22,5,104,4),
 (23,5,112,2),
 (24,7,103,2),
-(25,7,104,1);
+(25,7,104,1),
+(26,2,114,4);
 
 /*Table structure for table `student` */
 
@@ -61,24 +78,24 @@ CREATE TABLE `student` (
 
 insert  into `student`(`id`,`gpa`,`introduction`,`teacher_id`) values 
 (2,'3.28','我是一个热情开朗的女生，随和，自信，乐于助人，有责任心。',100),
-(3,'4.10','我是个有想象力的人，对什么都很好奇。',0),
-(4,'4.35','我求知欲较强，尤其喜欢历史、文学、人文地理等学科。',0),
-(5,'3.87','我深深迷恋古人所说的读万卷书，行万里路。因此我一爱看书，二爱旅行。',100),
-(6,'3.57','我对生活充满希望，因为我始终相信只要自己不放弃，我的这一生一定会活得有意义的。',0),
-(7,'4.03','我的生活非常充实，我学习很努力，其中多次拿了学校的奖学金。',0),
-(8,'4.00','大学不但让我学会了如何思考，学习的方法，还让我看到了实践的重要性。',0),
-(9,'3.88','我的一大特长爱好是设计，我积极地参加各种活动和设计大赛，曾在科技文化节中荣获节徽设计大赛二等奖。',0),
-(10,'2.67','我努力提升自我素质，全面锻炼自己，扎实完成自己的学习任务。',0),
-(11,'3.66','我经过自己的不懈努力连续三年获得校\"二等\"学习奖学金，\"优秀学生干部\"，\"优秀大学生\"等荣誉。',0),
-(12,'2.94','我坚信兴趣是最好的老师，不管今后在学业或科研上遇到什么困难，都要想尽办法克服。',0),
-(13,'3.45','我希望通过专业系统的学习，能够用自己所学的知识去解决实际问题，做出好成绩。',0),
-(14,'3.56','我已对我的学习做好了规划，我已经让自己做好了全面的准备。',0),
-(15,'3.22','我的大学生活让我学到了很多，很多的实践让我从从各个方面提高了自己。',0),
-(16,'2.45','我热爱计算机，希望未来能在计算机领域有所成就。',0),
-(17,'3.64','我现在是卓越教师班的学生，希望未来能成为一名卓越的老师，一直为此努力。',0),
-(18,'3.69','支教是我的梦想，我从高中开始就不放过每一个能接触支教的机会，未来我一定要去支教。',0),
-(19,'3.78','敲代码对我来说是件快乐的事，我闲着没事就会去看看代码。',0),
-(20,'3.26','我善于交际，善于管理，现任本班班长和团学负责人，有一定的组织能力。',0);
+(3,'4.10','我是个有想象力的人，对什么都很好奇。',100),
+(4,'4.35','我求知欲较强，尤其喜欢历史、文学、人文地理等学科。',101),
+(5,'3.87','我深深迷恋古人所说的读万卷书，行万里路。因此我一爱看书，二爱旅行。',101),
+(6,'3.57','我对生活充满希望，因为我始终相信只要自己不放弃，我的这一生一定会活得有意义的。',101),
+(7,'4.03','我的生活非常充实，我学习很努力，其中多次拿了学校的奖学金。',102),
+(8,'4.00','大学不但让我学会了如何思考，学习的方法，还让我看到了实践的重要性。',102),
+(9,'3.88','我的一大特长爱好是设计，我积极地参加各种活动和设计大赛，曾在科技文化节中荣获节徽设计大赛二等奖。',103),
+(10,'2.67','我努力提升自我素质，全面锻炼自己，扎实完成自己的学习任务。',103),
+(11,'3.66','我经过自己的不懈努力连续三年获得校\"二等\"学习奖学金，\"优秀学生干部\"，\"优秀大学生\"等荣誉。',103),
+(12,'2.94','我坚信兴趣是最好的老师，不管今后在学业或科研上遇到什么困难，都要想尽办法克服。',103),
+(13,'3.45','我希望通过专业系统的学习，能够用自己所学的知识去解决实际问题，做出好成绩。',103),
+(14,'3.56','我已对我的学习做好了规划，我已经让自己做好了全面的准备。',103),
+(15,'3.22','我的大学生活让我学到了很多，很多的实践让我从从各个方面提高了自己。',104),
+(16,'2.45','我热爱计算机，希望未来能在计算机领域有所成就。',105),
+(17,'3.64','我现在是卓越教师班的学生，希望未来能成为一名卓越的老师，一直为此努力。',105),
+(18,'3.69','支教是我的梦想，我从高中开始就不放过每一个能接触支教的机会，未来我一定要去支教。',105),
+(19,'3.78','敲代码对我来说是件快乐的事，我闲着没事就会去看看代码。',106),
+(20,'3.26','我善于交际，善于管理，现任本班班长和团学负责人，有一定的组织能力。',106);
 
 /*Table structure for table `sys_user` */
 
@@ -102,8 +119,8 @@ CREATE TABLE `sys_user` (
 
 insert  into `sys_user`(`id`,`username`,`password`,`nickname`,`email`,`phone`,`address`,`create_time`,`avatar_url`,`role`) values 
 (1,'admin','admin','管理员','admin@qq.com','8208208820','北京','2022-04-29 00:20:23',NULL,'ROLE_ADMIN'),
-(2,'王慧','123','小王','9104390@qq.com','15469387569','北京','2022-04-29 12:09:45',NULL,'学生'),
-(3,'黄文宇','123','小黄','934859@qq.com','15893750387','浙江省杭州市','2022-04-29 12:11:08','cccccc','学生'),
+(2,'王慧','123','王慧','9104390@qq.com','15469387569','北京','2022-04-29 12:09:45',NULL,'学生'),
+(3,'黄文宇','123','黄文宇','934859@qq.com','15893750387','浙江省杭州市','2022-04-29 12:11:08','cccccc','学生'),
 (4,'吴彦祖','123','吴彦祖','12948928135@163.com','13346523780','上海','2022-06-18 03:00:00',NULL,'学生'),
 (5,'张伟','123','张伟','124474663@qq.com','13522557890','中山','2022-06-18 03:06:50',NULL,'学生'),
 (6,'王芳','123','王芳','12343475@qq.com','13526793567','浙江省绍兴市','2022-06-18 05:34:55',NULL,'学生'),
@@ -154,10 +171,10 @@ CREATE TABLE `teacher` (
 /*Data for the table `teacher` */
 
 insert  into `teacher`(`id`,`title`,`enrollment`,`requirement`,`introduction`) values 
-(100,'讲师',223749,'人品正直，热爱学习','于2008年取得英国计算机科学博士学位，发表多篇期刊和会议文章。现指导多名研究生从事计算机科学、教育技术学和神经科学等方面的教学和研究工作。'),
-(101,'副教授',996,'负责任，擅长计算机知识','已入选浙江省高校中青年学科带头人、浙江省151人才工程和浙江工业大学青年英才支持计划。'),
+(100,'讲师',2,'人品正直，热爱学习','于2008年取得英国计算机科学博士学位，发表多篇期刊和会议文章。现指导多名研究生从事计算机科学、教育技术学和神经科学等方面的教学和研究工作。'),
+(101,'副教授',3,'负责任，擅长计算机知识','已入选浙江省高校中青年学科带头人、浙江省151人才工程和浙江工业大学青年英才支持计划。'),
 (102,'讲师',2,'有进取心，乐于助人，有管理能力','致力于特殊教育信息化和职业教育信息化，重点开展职业院校智慧教育和孤独症儿童早期干预研究。'),
-(103,'教授',7,'在计算机领域有所成就，可以潜下心来敲代码','在国内权威期刊发表学术论文，研究成果获得浙江省科学技术进步奖和浙江省高校科研成果奖。'),
+(103,'教授',6,'在计算机领域有所成就，可以潜下心来敲代码','在国内权威期刊发表学术论文，研究成果获得浙江省科学技术进步奖和浙江省高校科研成果奖。'),
 (104,'教授',1,'有组织能力','主持浙江省高校课堂教学改革项目1项，承担6门教育技术学专业本科生和研究生骨干课程的教学任务。'),
 (105,'教授',3,'有想象力，有求知欲','主持1项省教科规划课题、参编教材1本，2011年获浙江省委省政府颁发的“优秀科技特派员”称号。'),
 (106,'副教授',4,'有一定的承受能力，有耐心，热爱祖国','连续四届参加校青年教师教学技能比赛，获校优秀奖、院一等奖、二等奖、三等奖各1次。'),
@@ -189,33 +206,74 @@ end */$$
 
 DELIMITER ;
 
-/*!50106 set global event_scheduler = 1*/;
+/* Procedure structure for procedure `student_assign` */
 
-/* Event structure for event `student_assign` */
-
-/*!50106 DROP EVENT IF EXISTS `student_assign`*/;
+/*!50003 DROP PROCEDURE IF EXISTS  `student_assign` */;
 
 DELIMITER $$
 
-/*!50106 CREATE DEFINER=`root`@`localhost` EVENT `student_assign` ON SCHEDULE AT '2022-06-23 08:02:00' ON COMPLETION PRESERVE DISABLE DO BEGIN
-	    DECLARE teacherId Int;
-        DECLARE studentId Int;
-        DECLARE studentNum Int;
-        DECLARE i Int;
+/*!50003 CREATE DEFINER=`root`@`localhost` PROCEDURE `student_assign`()
+BEGIN
+DECLARE teacherId INT;
+        DECLARE studentId INT;
+        DECLARE studentNum INT;
+        DECLARE i INT;
         SET i=0;
-        SELECT count(*) INTO studentNum
+        SELECT COUNT(*) INTO studentNum
         FROM student s
         WHERE s.teacher_id = 0;
         WHILE
          i < studentNum DO
             SELECT s.id INTO studentId FROM student s WHERE s.teacher_id = 0 LIMIT 1;
-            SELECT t.id INTO teacherId FROM teacher t WHERE t.enrollment-(select count(*) from student where teacher_id=t.id)>0 LIMIT 1;
+            SELECT t.id INTO teacherId FROM teacher t WHERE t.enrollment-(SELECT COUNT(*) FROM student WHERE teacher_id=t.id)>0 LIMIT 1;
             UPDATE student SET student.teacher_id = teacherId WHERE student.id = studentId;
             
             SET i = i + 1;
       END WHILE;
-	END */$$
+    END */$$
 DELIMITER ;
+
+/*Table structure for table `mutual_selection_result` */
+
+DROP TABLE IF EXISTS `mutual_selection_result`;
+
+/*!50001 DROP VIEW IF EXISTS `mutual_selection_result` */;
+/*!50001 DROP TABLE IF EXISTS `mutual_selection_result` */;
+
+/*!50001 CREATE TABLE  `mutual_selection_result`(
+ `id` int ,
+ `username` varchar(50) ,
+ `nickname` varchar(50) ,
+ `teacher_id` int 
+)*/;
+
+/*Table structure for table `result` */
+
+DROP TABLE IF EXISTS `result`;
+
+/*!50001 DROP VIEW IF EXISTS `result` */;
+/*!50001 DROP TABLE IF EXISTS `result` */;
+
+/*!50001 CREATE TABLE  `result`(
+ `学号` varchar(50) ,
+ `姓名` varchar(50) ,
+ `导师姓名` varchar(50) ,
+ `导师邮箱` varchar(50) 
+)*/;
+
+/*View structure for view mutual_selection_result */
+
+/*!50001 DROP TABLE IF EXISTS `mutual_selection_result` */;
+/*!50001 DROP VIEW IF EXISTS `mutual_selection_result` */;
+
+/*!50001 CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `mutual_selection_result` AS select `sy`.`id` AS `id`,`sy`.`username` AS `username`,`sy`.`nickname` AS `nickname`,`st`.`teacher_id` AS `teacher_id` from (`sys_user` `sy` join `student` `st` on((`sy`.`id` = `st`.`id`))) where (`sy`.`role` = '学生') */;
+
+/*View structure for view result */
+
+/*!50001 DROP TABLE IF EXISTS `result` */;
+/*!50001 DROP VIEW IF EXISTS `result` */;
+
+/*!50001 CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `result` AS select `r`.`username` AS `学号`,`r`.`nickname` AS `姓名`,`s`.`nickname` AS `导师姓名`,`s`.`email` AS `导师邮箱` from (`mutual_selection_result` `r` join `sys_user` `s`) where (`s`.`id` = `r`.`teacher_id`) */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
